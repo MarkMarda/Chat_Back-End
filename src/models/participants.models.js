d3bffd026286336dea6483832911c81ee8001267
Marda <marda_da-capo@hotmail.com>
2022-10-28 05:51:25 -0500

    participants.models created

--- src/models/participants.models.js
+++ src/models/participants.models.js
@@ -2,13 +2,38 @@ const {DataTypes} = require("sequelize");
 
 const db = require("../utils/database");
 
+const Chats = require("./chats.models");
+
+const Users = require("./users.models");
+
+
+
 const Participants = db.define("participants", {
 
   id: {
     type: DataTypes.UUID,
     primaryKey: true,
     allowNull: false
+  },
+  userId: {
+    type: DataTypes.UUID,
+    allowNull: false,
+    field: "user_id",
+    references: {
+      key: "id",
+      model: Users
+    }
+  },
+  chatId: {
+    type: DataTypes.UUID,
+    allowNull: false,
+    field: "chat_id",
+    references: {
+      key: "id",
+      model: Chats
+    }
   }
+
 });
 
 module.exports = Participants;
